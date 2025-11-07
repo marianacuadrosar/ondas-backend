f5e20d502b692ad4a7bd55d39d4798196f5f4139
marianacuadrosar <marianacuar@unisabana.edu.co>
2025-11-07 07:28:17 -0500

    Initial commit

---
+++ src/main/java/com/hidroterapia/ondas/HidroterapiaOndasApplication.java
@@ -0,0 +1,13 @@
+package com.hidroterapia.ondas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidroterapiaOndasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidroterapiaOndasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hidroterapia/ondas/HidroterapiaOndasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidroterapia.ondas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HidroterapiaOndasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
